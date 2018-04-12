0ca8273145d7eb19e54177b88b41972a01214f6c
Erick Gomez Lorenzana <erickg.lorenzana@gmail.com>
2018-04-11 20:50:56 -0500

    tutorial in nav stack

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation';
 import LoginStack from './src/nav/loginStack';
 import DrawerStack from './src/nav/drawerStack';
+import StartTutorial from './src/components/startTutorial';
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
   loginStack: { screen: LoginStack },
+  tutorial: { screen: StartTutorial},
   drawerStack: { screen: DrawerStack }
 }, {
   // Default config for all screens
